64a3d2e379fbfe91112a83d541f2e39d40b3ccc0
pollitosabroson <alejandro@alejandro-23-b330la.(none)>
2014-05-01 12:37:48 -0500

    cambio unser psy

--- frente/settings.py
+++ frente/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'frente',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'frente',
+        'USER': 'data4',
         'PASSWORD': '123',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '', 
